af1e95c2ddc2eb983fae48dc9f85895bdd117262
Annu <143826991+Annu-200@users.noreply.github.com>
2026-07-01 18:09:50 +0530

    write and append method

---
+++ 03_fundamental_of_nodejs/fs-module/write-append/copy.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import fs, { readFile, writeFile } from 'node:fs/promises'
+import path from 'node:path'
+const file = process.argv
+const destination = file[3]
+const mainImg = file[2]
+const img = await readFile(mainImg)
+
+const filename = path.basename(mainImg)
+const finalPath = path.join(destination, filename)
+
+
+// console.log(img)
+await writeFile(finalPath, img)
+console.log("file copy successfuly")
+
+//task
+
+// fs.writeFile("D:\\Annu-Downloads\\extra space\\Desktop\\file-1.txt", "write now learning fs module write and append method")
+
+// const data = await readFile("D:\\Annu-Downloads\\extra space\\Desktop\\file-1.txt", "utf-8")
+
+// console.log(data)
