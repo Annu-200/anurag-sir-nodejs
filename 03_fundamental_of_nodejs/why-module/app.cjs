bf25dfbe99aadd0324e60a0f744c3c5477d69908
Annu <143826991+Annu-200@users.noreply.github.com>
2026-07-01 18:06:57 +0530

    Add CommonJS module import and console log

---
+++ 03_fundamental_of_nodejs/why-module/app.cjs
@@ -0,0 +1,18 @@
+// import fs from "node:fs/promises"
+const cjs = require('./why-modules.cjs')
+
+// // const a = readFileSync('./index.html')
+// const a = await fs.readFile('./index.html')
+
+// console.log(a.toString())
+// // console.log(a)
+// console.log('end')
+// import num from './common.js'
+// import sum from './why-modules.js'
+// console.log(sum(3, 4, 5));
+// console.log(typeof require)
+
+// console.log(sum(5,7))
+// console.log(Into(3,6))
+
+console.log(cjs)
