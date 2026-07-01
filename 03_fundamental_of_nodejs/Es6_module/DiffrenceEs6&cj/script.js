c6715099a4ed6d051ea519c98a96dc46cc0d8f80
Annu <143826991+Annu-200@users.noreply.github.com>
2026-07-01 18:02:37 +0530

    Add files via upload

---
+++ 03_fundamental_of_nodejs/Es6_module/DiffrenceEs6&cj/script.js
@@ -0,0 +1,9 @@
+// console.log('running script.js')
+
+
+// export default  'script'
+
+// import thridFirst from './thridFirst.js';
+const response = await fetch('https://jsonplaceholder.typicode.com')
+
+console.log(response)
---
+++ 03_fundamental_of_nodejs/Es6_module/DiffrenceEs6&cj/thridFirst.js
@@ -0,0 +1,3 @@
+export default 'thirdFirst'
+
+console.log('third but first')
