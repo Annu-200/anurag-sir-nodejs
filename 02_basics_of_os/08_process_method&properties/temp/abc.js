80d7cbbafcc1baaaa2f5a520ddfb245051f4fb7e
Annu <143826991+Annu-200@users.noreply.github.com>
2026-07-01 17:32:54 +0530

    Add abc.js to demonstrate process properties

---
+++ 02_basics_of_os/08_process_method&properties/temp/abc.js
@@ -0,0 +1,7 @@
+console.log(process.argv)
+console.log(process.chdir("./temp"))
+console.log(process.env())
+ console.log(process.arch())  // --> process architecture system X64
+ console.log(process.cwd())   // --> current working directory
+ console.log(process.version)  // --> node version
+  console.log(process.versions) // --> node version details
