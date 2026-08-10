aaf9d08a7743951150f099b21d8038338032d78c
Annu <143826991+Annu-200@users.noreply.github.com>
2026-08-10 18:51:59 +0530

    Add Uint8Array, Uint16Array, and Uint32Array examples

---
+++ 05_Buffer_in_nodejs/03_type-Array/app.js
@@ -0,0 +1,11 @@
+const a = new ArrayBuffer(4)
+
+const array8 = new Uint8Array(a)
+const array16 = new Uint16Array(a)
+const array32 = new Uint32Array(a)
+
+const uInt = new  Uint8Array(1.9 * 1024 * 1024 * 1024)
+
+for (let i = 0; i < buffer.length; i++) {
+   uInt
+}
